30de7f5c1eae59c9eaa318fbe6bffc82d6817236
Sagar Khatri <sagarkhatri986@gmail.com>
2023-03-04 17:48:17 -0500

    added bootstrap

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-window.onload = function () {
-  let cartList = document.getElementsByClassName("cartLink");
-  for (let cart of cartList) {
-    cart.addEventListener("click", addToCart);
-  }
+window.onload = function() {
+    let cartList = document.getElementsByClassName("cartLink");
+    for (let cart of cartList) {
+        cart.addEventListener("click", addToCart);
+    }
 };
 
 function addToCart(e) {
-  e.preventDefault();
-  let coun = JSON.parse(window.localStorage.getItem("cart"));
-  let count = coun != null ? ++coun : 1;
-  window.localStorage.setItem("cart", count);
-  document.getElementById("cartCount").innerHTML = count;
-  console.log("clicked");
+    e.preventDefault();
+    let coun = JSON.parse(window.localStorage.getItem("cart"));
+    let count = coun != null ? ++coun : 1;
+    window.localStorage.setItem("cart", count);
+    document.getElementById("cartCount").innerHTML = count;
+    console.log("clicked");
 }
